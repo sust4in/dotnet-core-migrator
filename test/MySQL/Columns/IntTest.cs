4373f650d3e27a424e429724a7fe08b497e8f523
Özgür Adem Işıklı <ozgur@polisoft.com.tr>
2017-01-31 13:23:14 +0300

    Test folders were restructured.

---
+++ test/MySQL/Columns/IntTest.cs
@@ -0,0 +1,26 @@
+﻿using Ozziest.Columns;
+using Ozziest.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace test.MySQL.Columns
+{
+    public class IntTest
+    {
+
+        private MySQLGenerator generator = new MySQLGenerator();
+
+        [Fact]
+        public void TestInt()
+        {
+            IColumn column = new Int("user_id");
+            column.SetNotNull();
+            column.SetAutoIncrement();
+            Assert.Equal("`user_id` INT NOT NULL AUTO_INCREMENT", generator.Int(column));
+        }
+
+    }
+}
---
+++ test/MySQL/Columns/VarCharTest.cs
@@ -0,0 +1,38 @@
+﻿using Ozziest.Columns;
+using Ozziest.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace test.MySQL.Columns
+{
+    public class VarCharTest
+    {
+
+        private MySQLGenerator generator = new MySQLGenerator();
+
+        [Fact]
+        public void TestVarChar()
+        {
+            // Simple varchar
+            IColumn column = new VarChar("users", 100);
+            Assert.Equal("`users` VARCHAR(100)", generator.VarChar(column));
+
+            // Advanced var char
+            column = new VarChar("nickname", 50);
+            column.SetNotNull();
+            column.SetUnique();
+            Assert.Equal("`nickname` VARCHAR(50) NOT NULL UNIQUE", generator.VarChar(column));
+        }
+
+        [Fact]
+        public void TestVarCharExceptionAutoIncrement()
+        {
+            IColumn column = new VarChar("users", 100);
+            Assert.Throws<Exception>(() => column.SetAutoIncrement());
+        }
+
+    }
+}
