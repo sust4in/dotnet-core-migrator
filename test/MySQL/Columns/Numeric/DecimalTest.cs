5c23fae0b4de09259a1027d2f5f329f5724b9659
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2017-02-07 21:06:32 +0300

    Tests were updated

---
+++ test/MySQL/Columns/Numeric/DecimalTest.cs
@@ -0,0 +1,22 @@
+﻿using Ozziest.Columns;
+using Ozziest.Columns.Numeric;
+using Ozziest.Generators;
+using Xunit;
+
+namespace test.MySQL.Columns.Numeric
+{
+    public class DecimalTest
+    {
+
+        private MySQLGenerator generator = new MySQLGenerator();
+
+        [Fact]
+        public void TestDecimal()
+        {
+            IColumn column = new DecimalTest("my_decimal_field", 5, 2);
+            column.SetNotNull();
+            Assert.Equal("`my_decimal_field` DECIMAL(5, 2) NOT NULL", generator.ToSQL(column));
+        }
+
+    }
+}
