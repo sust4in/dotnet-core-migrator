901dbd0d8b2ca073bfde754c0397dc2ac6b17df0
muratgucuk <muratgucuk54@gmail.com>
2017-02-11 00:36:59 +0200

    Timestamp data type was defined and test completed.

---
+++ test/MySQL/Columns/DateTime/TimestampTest.cs
@@ -0,0 +1,25 @@
+﻿using Ozziest.Columns;
+using Ozziest.Columns.DateTime;
+using Ozziest.Generators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace test.MySQL.Columns.DateTime
+{
+    public class TimestampTest
+    {
+        private MySQLGenerator generator = new MySQLGenerator();
+
+        [Fact]
+        public void TestDate()
+        {
+            IColumn column = new Timestamp("created_atStamp", 6);
+            column.Unique();
+            column.NotNull();
+            Assert.Equal("`created_atStamp` TIMESTAMP(6) NOT NULL UNIQUE", generator.ToSQL(column));
+        }
+    }
+}
